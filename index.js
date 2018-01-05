a6a75b57ef3d8664e0b650fa4f7e0e56d16f7462
henain.xu <404042355@qq.com>
2018-01-05 10:57:48 +0800

    4L 临时版本

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ var loadAllAreaUrl;
 function AreaChooseAjax(options) {
 
   childUrl = options.childUrl || '/view/region/getChild';
-  loadAllAreaUrl = options.loadAllAreaUrl || '/view/region/loadAllArea?id=';
+  if (options.chinaType == 1) {
+    loadAllAreaUrl = options.loadAllAreaUrl || '/view/region/loadAllArea?chinaType=1&id=';
+  } else {
+    loadAllAreaUrl = options.loadAllAreaUrl || '/view/region/loadAllArea?id=';
+  }
 
   if ((typeof options.pid !== 'undefined') && (typeof options.lv !== 'undefined')) {
-    throw "不允许同时定义pid跟lv"
+    throw '不允许同时定义pid跟lv';
   }
   if ((typeof options.pid === 'undefined') && (typeof options.lv === 'undefined')) {
     options.lv = 2;
@@ -26,8 +30,7 @@ function AreaChooseAjax(options) {
   this.init(this.opt.id);
 }
 
-
-AreaChooseAjax.prototype.init = function (id) {
+AreaChooseAjax.prototype.init = function(id) {
   var self = this;
   this.domArr = [];
   // 初始值
@@ -35,54 +38,10 @@ AreaChooseAjax.prototype.init = function (id) {
     $.ajax({
       url: loadAllAreaUrl + id,
       data: {
-        limitLevel: self.opt.limitLevel
+        limitLevel: self.opt.limitLevel,
       },
-      type: 'get'
-    })
-      .success(function (result) {
-        if (!result.success) {
-          if (layer) {
-            layer.msg(result.msg);
-          } else {
-            alert(result.msg);
-          }
-          return;
-        }
-        $.isFunction(self.opt.init)&&self.opt.init.call(self);
-        if (result.data && result.data.length) {
-          $.each(result.data, function (key, item) {
-            if (!item.length)return;
-            var node = ['<select name="" id="" data-level="' + item[0].level + '"><option value="">' + self.selectTxt + '</option>'];
-            $.each(item, function (k, val) {
-              if (val.isSelect) {
-                node.push('<option selected value="' + val.id + '">' + val.name + '</option>');
-              } else {
-                node.push('<option value="' + val.id + '">' + val.name + '</option>');
-              }
-            });
-            node.push('</select>');
-            $(self.el).append(node.join(' '));
-          })
-        }
-        $.isFunction(self.opt.inited)&&self.opt.inited.call(self);
-      })
-  } else {
-    self.getChild({
-      lv: self.opt.lv,
-      pId: self.opt.pid,
-      limitLevel: self.opt.limitLevel
-    })
-  }
-};
-AreaChooseAjax.prototype.getChild = function (params) {
-  var child;
-  var self = this;
-  $.ajax({
-    url: childUrl,
-    data: params,
-    type: 'get'
-  })
-    .success(function (result) {
+      type: 'get',
+    }).success(function(result) {
       if (!result.success) {
         if (layer) {
           layer.msg(result.msg);
@@ -91,35 +50,77 @@ AreaChooseAjax.prototype.getChild = function (params) {
         }
         return;
       }
-      child = result.data;
-
-      if (child && child.length) {
-        var node = ['<select name="" id="" data-level="' + child[0].level + '"><option value="">' + self.selectTxt + '</option>'];
-        $.each(child, function (key, item) {
-          node.push('<option value="' + item.id + '">' + item.name + '</option>');
+      $.isFunction(self.opt.init) && self.opt.init.call(self);
+      if (result.data && result.data.length) {
+        $.each(result.data, function(key, item) {
+          if (!item.length) return;
+          var node = ['<select name="" id="" data-level="' + item[0].level + '"><option value="">' + self.selectTxt + '</option>'];
+          $.each(item, function(k, val) {
+            if (val.isSelect) {
+              node.push('<option selected value="' + val.id + '">' + val.name + '</option>');
+            } else {
+              node.push('<option value="' + val.id + '">' + val.name + '</option>');
+            }
+          });
+          node.push('</select>');
+          $(self.el).append(node.join(' '));
         });
-        node.push('</select>');
-        $(self.el).append(node.join(' '));
-      } else {
-        $.isFunction(self.opt.nodata) && self.opt.nodata.call(null, params.pId);
       }
-    })
+      $.isFunction(self.opt.inited) && self.opt.inited.call(self);
+    });
+  } else {
+    self.getChild({
+      lv: self.opt.lv,
+      pId: self.opt.pid,
+      limitLevel: self.opt.limitLevel,
+    });
+  }
+};
+AreaChooseAjax.prototype.getChild = function(params) {
+  var child;
+  var self = this;
+  $.ajax({
+    url: childUrl,
+    data: params,
+    type: 'get',
+  }).success(function(result) {
+    if (!result.success) {
+      if (layer) {
+        layer.msg(result.msg);
+      } else {
+        alert(result.msg);
+      }
+      return;
+    }
+    child = result.data;
+
+    if (child && child.length) {
+      var node = ['<select name="" id="" data-level="' + child[0].level + '"><option value="">' + self.selectTxt + '</option>'];
+      $.each(child, function(key, item) {
+        node.push('<option value="' + item.id + '">' + item.name + '</option>');
+      });
+      node.push('</select>');
+      $(self.el).append(node.join(' '));
+    } else {
+      $.isFunction(self.opt.nodata) && self.opt.nodata.call(null, params.pId);
+    }
+  });
 };
 
-AreaChooseAjax.prototype.onChange = function (e) {
+AreaChooseAjax.prototype.onChange = function(e) {
   var
-    $target = $(e.target),
-    id = Number($target.val()),
-    self = this;
+      $target = $(e.target),
+      id = Number($target.val()),
+      self = this;
   $(e.target).nextAll('select').remove();
   if (!id) return;
   this.getChild({
     pId: id,
-    limitLevel: self.opt.limitLevel
-  })
+    limitLevel: self.opt.limitLevel,
+  });
 };
 
-AreaChooseAjax.prototype.bind = function (id) {
+AreaChooseAjax.prototype.bind = function(id) {
   // TODO: 只移除this.onChange
   $('body').off('change', this.el + ' select');
   $('body').on('change', this.el + ' select', this.onChange.bind(this));
@@ -127,20 +128,20 @@ AreaChooseAjax.prototype.bind = function (id) {
 
 /**解决ie8及以下 "bind()" 方法不兼容问题**/
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
     }
     var aArgs = Array.prototype.slice.call(arguments, 1),
-      fToBind = this,
-      fNOP = function () {
-      },
-      fBound = function () {
-        return fToBind.apply(this instanceof fNOP && oThis
-            ? this
-            : oThis,
-          aArgs.concat(Array.prototype.slice.call(arguments)));
-      };
+        fToBind = this,
+        fNOP = function() {
+        },
+        fBound = function() {
+          return fToBind.apply(this instanceof fNOP && oThis
+              ? this
+              : oThis,
+              aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
     fNOP.prototype = this.prototype;
     fBound.prototype = new fNOP();
     return fBound;
